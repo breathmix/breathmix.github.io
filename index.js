a17d204a36f72e85ee8b67c42fc5483c2d9c8eb6
breathmix <breathmix.team@gmail.com>
2018-04-05 01:54:18 +0300

    site updated

---
+++ index.js
@@ -0,0 +1,11 @@
+var App = function() {
+
+	var button = document.getElementById("button");
+	button.innerHTML = "bla bls";	
+
+	var element = document.getElementById("bla");
+	element.innerHTML="dsfd";
+}
+
+app = new App();
+
